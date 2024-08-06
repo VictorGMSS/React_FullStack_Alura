beed43bb39b8ef4f69a549c07e38bde293b39612
Victor Gabriel <victors.study@gmail.com>
2024-08-06 11:00:59 -0300

    adicionando modificações no front

---
+++ src/componentes/Pesquisa/dadosPesquisa.js
@@ -0,0 +1,10 @@
+import livro from '../../imagens/livro.png';
+
+export const livros = [
+    {nome: 'Liderança em Design', id: 1, src:livro},
+    {nome: 'JavaScript Assertivo', id: 2, src:livro},
+    {nome: 'Basico do NodeJS', id: 3, src:livro},
+    {nome: 'HTML5 avançado', id: 4, src:livro},
+    {nome: 'Aplicações do Python', id: 5, src:livro},
+    {nome: 'Conceitos de POO', id: 6, src:livro},
+]
